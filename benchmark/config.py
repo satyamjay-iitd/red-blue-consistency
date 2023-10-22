61bf25c11edf683cabe419014f3b0fffb4fbb24d
satyam <satyamjay030@gmail.com>
2023-10-22 14:00:23 +0530

    debug config

--- benchmark/config.py
+++ benchmark/config.py
@@ -6,8 +6,7 @@ load_dotenv('../.env')
 RDS_MASTER_PORT = os.environ['RDS_MASTER_PORT']
 RDS_REPLICA1_PORT = os.environ['RDS_REPLICA1_PORT']
 RDS_REPLICA2_PORT = os.environ['RDS_REPLICA2_PORT']
-RDS_REP_IN_SYNC = bool(os.environ['RDS_REP_IN_SYNC'])
-
+RDS_REP_IN_SYNC = os.environ['RDS_REP_IN_SYNC'].lower() == 'true'
 RB_PORT1 = os.environ['RB_PORT1']
 
 BANK_INTEREST = 0.01
