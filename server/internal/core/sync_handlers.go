6f9b5fcce0a8b50b631296821eedfa40edbf03ac
Rohith Vishnumolakala <rohith.vishnu333@gmail.com>
2023-11-07 09:57:18 +0530

    make the hgetall sync red op

--- server/internal/core/sync_handlers.go
+++ server/internal/core/sync_handlers.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"encoding/json"
 	"errors"
 	"io"
 	"log"
@@ -25,6 +26,16 @@ func HandleSyncCommands(
 			return errors.New("SYNC_BANK_WRITE command requires 1 arguments")
 		}
 		resp = handleSyncBankWrite(isMaster, command.Args[0])
+	case "SYNC_HGETALL":
+		if len(command.Args) != 1 {
+			return errors.New("SYNC_HGETALL command requires 1 argument")
+		}
+		resp = handleSyncHGETALL(command.Args[0])
+	case "SYNC_HSETALL":
+		if len(command.Args) != 1 {
+			return errors.New("SYNC_HSETALL command requires 1 argument")
+		}
+		resp = handleSyncHSETALL(command.Args[0], c)
 	default:
 		return errors.New("unknown command")
 	}
@@ -35,8 +46,9 @@ func HandleSyncCommands(
 func handleSyncBankRead(isMaster bool) (resp []byte) {
 	if isMaster {
 		resp = []byte("Cannot call sync commands on master")
+	} else {
+		resp = []byte(strconv.FormatFloat(bank, 'f', -1, 64))
 	}
-	resp = []byte(strconv.FormatFloat(bank, 'f', -1, 64))
 	return resp
 }
 
@@ -50,4 +62,66 @@ func handleSyncBankWrite(isMaster bool, amt string) []byte {
 	}
 	bank = amtF
 	return OK
+}
+
+func handleSyncHGETALL(key string) []byte {
+	obj := Get(key)
+	if obj == nil {
+		hset := make(HsetInt)
+		return hset.GetAll()
+	} else if obj.Type != OBJ_TYPE_HASH_INT {
+		return []byte("Key is not an int hash")
+	}
+	hset := obj.Val.(HsetInt)
+	return hset.GetAll()
+}
+
+func handleSyncHSETALL(key string, c io.ReadWriter) []byte {
+	obj := Get(key)
+	// var hset HsetInt
+	if obj == nil {
+		// Create a new hash
+		// hset = make(HsetInt)
+	} else if obj.Type != OBJ_TYPE_HASH_INT {
+		return []byte("Key is not an int hash")
+	} else {
+		// hset = obj.Val.(HsetInt)
+	}
+
+	// Respond OK
+	_, err := c.Write(OK)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Read the complete data
+	data, err := readCompleteData(c)
+	if err != nil {
+		log.Println(err)
+	}
+
+	var newhset HsetInt
+	err = json.Unmarshal(data, &newhset)
+	if err != nil {
+		log.Println(err)
+	}
+
+	Set(key, NewObject(newhset, nil, OBJ_TYPE_HASH_INT))
+	return OK
+}
+
+func readCompleteData(c io.ReadWriter) ([]byte, error) {
+	var buf []byte = make([]byte, 1024)
+	var data = make([]byte, 0)
+	for {
+		n, err := c.Read(buf[:])
+		if err != nil && err != io.EOF {
+			return nil, err
+		}
+		data = append(data, buf[:n]...)
+		if n < 1024 || err == io.EOF {
+			break
+		}
+	}
+	return data, nil
 }
